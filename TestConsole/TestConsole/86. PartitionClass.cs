af3edbfaa9fe95936c71d1a44f87ba071fb6cb63
foxdave <foxdave@live.cn>
2021-01-03 15:29:37 +0800

    NO. 86

---
+++ TestConsole/TestConsole/86. PartitionClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class PartitionClass
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode result = new ListNode(int.MinValue);
+            ListNode oriPointer = head;
+            ListNode newPointer = result;
+            ListNode targetNode = null;
+            ListNode preTargetNode = null;
+
+            while (oriPointer != null)
+            {
+                ListNode newNode = new ListNode(oriPointer.val);
+
+                if (targetNode != null && oriPointer.val < x)
+                {
+                    preTargetNode.next = newNode;
+                    newNode.next = targetNode;
+                    preTargetNode = newNode;
+                }
+                else
+                {
+                    newPointer.next = newNode;
+                }
+
+                if (targetNode == null && oriPointer.val >= x)
+                {
+                    targetNode = newPointer.next;
+                    preTargetNode = newPointer;
+                }
+                if (newPointer.next != null)
+                {
+                    newPointer = newPointer.next;
+                }
+                oriPointer = oriPointer.next;
+            }
+
+            return result.next;
+        }
+    }
+}
--- TestConsole/TestConsole/Program.cs
+++ TestConsole/TestConsole/Program.cs
@@ -7,6 +7,14 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            ListNode head = new ListNode(2);
+            ListNode node1 = new ListNode(1);
+            ListNode node2 = new ListNode(3);
+            head.next = node1;
+            node1.next = node2;
+
+            int x = 2;
+            ListNode result = new PartitionClass().Partition(head, x);
             //int[] result = new MaxSlidingWindowClass().MaxSlidingWindow(new int[] { -7, -8, 7, 5, 7, 1, 6, 0 }, 4);
             //bool result = new CanPlaceFlowersClass().CanPlaceFlowers(new int[] { 1, 0, 0, 0, 0, 1 }, 2);
             //bool result = new IsIsomorphicClass().IsIsomorphic("ab", "aa");
