0843e7f2b769dca354a5ccd1489dcdae63cd3976
Justin <foxdave@live.cn>
2021-01-04 11:11:11 +0800

    NO. 509

---
+++ TestConsole/TestConsole/_509. FIBClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class FIBClass
+    {
+        public int FIB(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            return FIB(n - 1) + FIB(n - 2);
+        }
+    }
+}
---
+++ TestConsole/UnitTestProject/FIBClassTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConsole;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole.Tests
+{
+    [TestClass()]
+    public class FIBClassTests
+    {
+        [TestMethod()]
+        public void FIBTest1()
+        {
+            int n = 2;
+            int expectedResult = 1;
+            int actualResult = new FIBClass().FIB(n);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod()]
+        public void FIBTest2()
+        {
+            int n = 3;
+            int expectedResult = 2;
+            int actualResult = new FIBClass().FIB(n);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod()]
+        public void FIBTest3()
+        {
+            int n = 4;
+            int expectedResult = 3;
+            int actualResult = new FIBClass().FIB(n);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
