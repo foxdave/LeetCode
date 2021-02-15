84f0e8211516b3a872ad9166c27458ada61ccd1c
foxdave <foxdave@live.cn>
2021-02-15 09:22:43 +0800

    NO. 485 FindMaxConsecutiveOnes

---
+++ TestConsole/TestConsole/2021.2/_485. FindMaxConsecutiveOnesClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class FindMaxConsecutiveOnesClass
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int maxCOne = 0;
+            int currentCOne = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    currentCOne = 0;
+                }
+                if (nums[i] == 1)
+                {
+                    currentCOne++;
+                }
+                if (currentCOne > maxCOne)
+                {
+                    maxCOne = currentCOne;
+                }
+            }
+            return maxCOne;
+        }
+    }
+}
---
+++ TestConsole/UnitTestProject/2021.2/FindMaxConsecutiveOnesClassTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConsole;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole.Tests
+{
+    [TestClass()]
+    public class FindMaxConsecutiveOnesClassTests
+    {
+        [TestMethod()]
+        public void FindMaxConsecutiveOnesTest()
+        {
+            int[] nums = new int[] { 1, 1, 0, 1, 1, 1 };
+            int expectedResult = 3;
+            int actualResult = new FindMaxConsecutiveOnesClass().FindMaxConsecutiveOnes(nums);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
