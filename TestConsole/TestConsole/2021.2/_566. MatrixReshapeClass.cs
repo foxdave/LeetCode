568f4899862c9c1c2908849c28cec4e4a83fe483
foxdave <foxdave@live.cn>
2021-02-17 09:22:46 +0800

    NO. 566 MatrixReshape

---
+++ TestConsole/TestConsole/2021.2/_566. MatrixReshapeClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class MatrixReshapeClass
+    {
+        public int[][] MatrixReshape(int[][] nums, int r, int c)
+        {
+            int oriRow = nums.Length;
+            int oriColumn = nums[0].Length;
+            if (oriRow * oriColumn != r * c)
+            {
+                return nums;
+            }
+
+            //Fill data to a array
+            int[] data = new int[r * c];
+            for (int i = 0; i < oriRow; i++)
+            {
+                int[] rowNum = nums[i];
+                for (int j = 0; j < oriColumn; j++)
+                {
+                    data[i * oriColumn + j] = rowNum[j];
+                }
+            }
+
+            int[][] result = new int[r][];
+
+            for (int i = 0; i < r; i++)
+            {
+                int[] resRow = new int[c];
+                for (int j = 0; j < c; j++)
+                {
+                    resRow[j] = data[i * c + j];
+                }
+                result[i] = resRow;
+            }
+            return result;
+        }
+    }
+}
---
+++ TestConsole/TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TestConsole.StockSeries;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] nums = { new int[] { 1, 2 }, new int[] { 3, 4 } };
+            int[][] result = new MatrixReshapeClass().MatrixReshape(nums, 4, 1);
+        }
+    }
+}
