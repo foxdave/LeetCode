b8c39cdb95284a90bdca3cca017dd50ce8a81e4b
foxdave <foxdave@live.cn>
2021-02-28 11:39:02 +0800

    NO. 896 IsMonotonic

---
+++ TestConsole/TestConsole/2021.2/_896. IsMonotonicClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TestConsole
+{
+    public class IsMonotonicClass
+    {
+        public bool IsMonotonic(int[] A)
+        {
+            if (A.Length == 0 || A.Length == 1)
+            {
+                return true;
+            }
+            int length = A.Length;
+            if (A[0] > A[length - 1])
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    if (A[i] < A[i + 1])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            if (A[0] < A[length - 1])
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    if (A[i] > A[i + 1])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return A.Distinct().Count() == 1;
+        }
+    }
+}
