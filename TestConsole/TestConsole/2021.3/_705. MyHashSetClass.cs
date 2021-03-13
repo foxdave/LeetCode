2f158730099df2e368369f51d501a9b488d72c37
foxdave <foxdave@live.cn>
2021-03-13 09:36:44 +0800

    NO. 705 MyHashSet

---
+++ TestConsole/TestConsole/2021.3/_705. MyHashSetClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public class MyHashSet
+    {
+        List<int> baseData = new List<int>();
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+
+        }
+
+        public void Add(int key)
+        {
+            if (!baseData.Contains(key))
+            {
+                baseData.Add(key);
+            }
+        }
+
+        public void Remove(int key)
+        {
+            baseData.Remove(key);
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            return baseData.Contains(key);
+        }
+    }
+
+    /**
+     * Your MyHashSet object will be instantiated and called as such:
+     * MyHashSet obj = new MyHashSet();
+     * obj.Add(key);
+     * obj.Remove(key);
+     * bool param_3 = obj.Contains(key);
+     */
+}
